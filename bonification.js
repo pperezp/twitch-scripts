8c25dfbbfae867f7afb4cc5c359056663160807c
pperezp <patodeath@gmail.com>
2021-06-22 00:00:24 -0400

    Add get bonification points

--- bonification.js
+++ bonification.js
@@ -1,14 +1,24 @@
-function bonification() {
-    setInterval(function () {
+let interval;
+let oldPoints;
+
+function startBotBonification() {
+    interval = setInterval(function () {
         let divs = document.getElementsByTagName("div");
         let searchText = "¡Haz clic para hacerte con una bonificación!";
         let found;
 
         for (let i = 0; i < divs.length; i++) {
             if (divs[i].textContent == searchText) {
+                oldPoints = getBalancePoints();
                 console.log("Encontrado " + new Date());
                 found = divs[i];
                 found.children[0].children[0].children[0].children[0].click();
+
+                setTimeout(() => { 
+                    let currentPoints = getBalancePoints();
+                    console.log("Current points: " + currentPoints + " (+" + (currentPoints - oldPoints) + ")");
+                }, 1000);
+
                 break;
             }
         }
@@ -18,5 +28,17 @@ function bonification() {
         }
     }, (1000 * 60));
 }
-   
-bonification();
+
+function getBalancePoints(){
+    for(let div of document.getElementsByTagName("div")){
+        if(div.dataset.testSelector != undefined && div.dataset.testSelector == "balance-string"){
+            return Number.parseInt(div.children[0].innerHTML);
+        }
+    }
+}
+
+function stopInterval(){
+    clearInterval(interval);
+}
+
+startBotBonification();
